a335e2fc5fd679da67617714f374c22a65867e0f
Bastien <bastien.pouessel@epita.fr>
2023-12-10 16:36:02 +0100

    feat: calling the api of the rag chain in the gradio UI

--- gradio-ui/app.py
+++ gradio-ui/app.py
@@ -1,11 +1,42 @@
+"""
+Gradio UI for Mistral 7B with RAG
+"""
+
+# Author: Bastien Pouessel
+
+import logging
+
 import gradio as gr
+import requests
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def update_file(file):
+    # change file for
+    if file is not None:
+        requests.post("http://localhost:80/document", json={"path": file.name})
+
+        logging.debug(f"File was uploaded: {file.name}")
 
 
 def echo(message, history, file):
-    if file is None:
-        return message
-    else:
-        return file.name
+    # change file for
+    update_file(file)
+
+    # call the chain through the API
+    payload = {"input": message}
+    response = requests.post(
+        "http://localhost:80/chain/invoke",
+        json=payload,
+    )
+
+    logging.debug(f"Response: {response.json()}")
+
+    if "output" in response.json():
+        return response.json()["output"]
+
+    return None
 
 
 demo = gr.ChatInterface(
@@ -17,4 +48,4 @@ demo = gr.ChatInterface(
 )
 
 
-demo.launch(server_name="0.0.0.0", server_port=7860)
+demo.queue().launch(server_name="0.0.0.0", server_port=7860)
