e8a352ff223b99628a1bf6aad8eb633643e7c588
Bastien <bastien.pouessel@epita.fr>
2023-12-10 15:31:42 +0100

    feat: changing document an api call

--- chatbot-rag/serve.py
+++ chatbot-rag/serve.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from langserve import add_routes
 from llm_retrival import generate_rag_chain, load_pdf, store_vector
+from pydantic import BaseModel
+
+
+class Document(BaseModel):
+    """
+    Document path model.
+    """
+
+    path: str
+
 
 # FastAPI app
 app = FastAPI(
@@ -9,15 +19,30 @@ app = FastAPI(
     description="API server in order to retrieve information from a document",
 )
 
-# Load the document and generate the chain
-document_path = "data/thesis.pdf"
-all_splits = load_pdf(document_path)
-retriever = store_vector(all_splits)
-chain = generate_rag_chain(retriever)
+app.all_splits = load_pdf("data/thesis.pdf")
+app.retriever = store_vector(app.all_splits)
+app.chain = generate_rag_chain(app.retriever)
+
+
+@app.post("/document")
+def set_document(document: Document) -> Response:
+    """
+    Set the document to use for the chatbot.
+
+    Args:
+        document_path (str): path to the document
+    """
+
+    # Split the document into chunks of text set a new retriever and chain
+    app.all_splits = load_pdf(document.path)
+    app.retriever = store_vector(app.all_splits)
+    app.chain = generate_rag_chain(app.retriever)
+    return {"message": "Document set"}
+
 
 # Add routes to the app using the chain
 add_routes(
     app,
-    chain,
+    app.chain,
     path="/chain",
 )
